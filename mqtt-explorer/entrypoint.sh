4f392725cc30fb753eab4a3fef6ee246c9ea8483
SmeagolWorms4 <smeagolworms4@gmail.com>
2023-08-08 13:51:09 +0200

    Fix

--- mqtt-explorer/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-[ -e "/data/settings.json" ] && echo '{"ConnectionManager_connections":{"mqtt.eclipse.org":{"configVersion":1,"certValidation":true,"clientId":"mqtt-explorer-ce4f0fc6","id":"mqtt.eclipse.org","name":"homeassitant","encryption":false,"subscriptions":[{"topic":"#","qos":0},{"topic":"$SYS/#","qos":0}],"type":"mqtt","host":"core-mosquitto.local.hass.io","port":1883,"protocol":"mqtt","changeSet":{"password":""},"username":"","password":""}}}' > /data/settings.json
-
-"$@"
